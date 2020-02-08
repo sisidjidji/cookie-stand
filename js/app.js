515ab25b6f50bb5cdc5ba84b7dfcb192bda6f734
sisidjidji <sahr1560@gmail.com>
2020-02-08 17:22:07 -0600

    first row done

--- js/app.js
+++ js/app.js
@@ -2,131 +2,146 @@
 
 
 var storesArray=[];// array that store the objects 
-var time= ['6am :','7am :','8am :','9am :','10am :','11am :','12am :','1pm :','2pm :','3pm :','4pm :','5pm :','6pm :','7pm :','total:']
+ var time= ['6am ','7am ','8am ','9am ','10am ','11am ','12am ','1pm ','2pm ','3pm ','4pm ','5pm ','6pm ','7pm ','Total'];
 
-var store1={  // creating store1 object
-    storeName:'Seattle',
-    min:23,
-    max:65,
-    avg:6.3,
-    cookiesHour:[],  // an array for the number of cookies sold per hour
-    dailyTotal:0, 
-    setcookies: function() { // method that return a rundom number of cookies per hour 
- for (var i=0;i<14;i++){
-    this.cookiesHour[i]=getRandomInt(this.min, this.max,this.avg);
-    this.dailyTotal+=this.cookiesHour[i]; 
-           }
 
-           }
-           }
+
+
+
+ function store(name,min,max,avg) { // creating a constructor function 
+   this.name=name;
+   this.min=min;
+   this.max=max;
+   this.avg=avg;
+   this.cookiesHour=[];
+   this.dailyTotal=0;
+   
+}
  
-store1.setcookies(); // call the function setcookies of the store1 object
-storesArray.push(store1); // push the object store1 in to the storesArray
 
-var store2={  // creating  store2 object
-    storeName:'Tokyo',
-    min:3,
-    max:24,
-    avg:1.2,
-    cookiesHour:[],
-    dailyTotal:0,
-    setcookies: function() {
- for (var i=0;i<14;i++){
-    this.cookiesHour[i]=getRandomInt(this.min, this.max,this.avg);
-    this.dailyTotal+=this.cookiesHour[i];
-            }
-        }
-        }
-
-store2.setcookies();// call the function setcookies of the store2 object
-storesArray.push(store2);// push the object store2 in to the storesArray
-
-var store3={  // store3 object
-    storeName:'Dubai',
-    min:11,
-    max:38,
-    avg:3.7,
-    cookiesHour:[],
-    dailyTotal:0,
-    setcookies: function() {
-    for (var i=0;i<14;i++){
-     this.cookiesHour[i]=getRandomInt(this.min, this.max,this.avg);
-     this.dailyTotal+=this.cookiesHour[i];
-           }
-        }
-        }
-store3.setcookies();// call the function setcookies of the store3 object
-storesArray.push(store3);// push the object store3 in to the storesArray
-
- var store4={  // store4 object
-    storeName:'paris',
-    min:20,
-    max:38,
-    avg:2.3,
-    cookiesHour:[],
-    dailyTotal:0,
-    setcookies: function() {
-    for (var i=0;i<14;i++){
+store.prototype.setcookies=function(min,max,avg) { // set a prototype function that gonna return an array  of rundom numbers of cookies sold per hour
+   for (var i=0;i<14;i++){
       this.cookiesHour[i]=getRandomInt(this.min, this.max,this.avg);
+      
+              }
+             }
+             
+store.prototype.setTotal= function(){// set a prototype function that gonna return a total cookies sold per day
+   for (var i=0;i<14;i++){
+
       this.dailyTotal+=this.cookiesHour[i];
-        }
-         }
-       }
-            
- store4.setcookies();// call the function setcookies of the store4 object
- storesArray.push(store4);// push the object store4 in to the storesArray
+              }
+  }
+ function getRandomInt(min, max,avg)  {      // Fonction that return a rundom number of cookies sold per hours
+               var customer = Math.floor(Math.random() * (max - min)) + min;
+                return Math.floor(customer*avg);
+              }
 
- var store5={  // store5 object
-    storeName:'Lima',
-    min:2,
-    max:16,
-    avg:4.6,
-    cookiesHour:[],
-    dailyTotal:0,
-    setcookies: function() {
-     for (var i=0;i<14;i++){
-    this.cookiesHour[i]=getRandomInt(this.min, this.max,this.avg);
-     this.dailyTotal+=this.cookiesHour[i];
-        }
-        }
-        }
 
- store5.setcookies();// call the function setcookies of the store5 object
- storesArray.push(store5);// push the object store5 in to the storesArray
+var store1= new store('Seattle','23','65','6.3');  // creating new instance of store named store1
+store1.setcookies(); // call the function setcokies
+store1.setTotal();   // call the function setTotal
+storesArray.push(store1);
+var store2= new store('Tokyo','3','24','1.2');// creating new instance of store named store2
+store2.setcookies();
+store2.setTotal();
+storesArray.push(store2);
+var store3= new store('Dubai','11','38','3.7');// creating new instance of store named store3
+store3.setcookies();
+store3.setTotal();
+storesArray.push(store3);
+var store4= new store('Paris','20','38','2.3');// creating new instance of store named store4
+store4.setcookies();
+store4.setTotal();
+storesArray.push(store4);
+var store5= new store('Lima','2','16','4.6');// creating new instance of store named store5
+store5.setcookies();
+store5.setTotal();
+storesArray.push(store5);
 
-    function getRandomInt(min, max,avg)  {      // Fonction that return a rundom number of cookies sold per hours
-       var customer = Math.floor(Math.random() * (max - min)) + min;
-        return Math.floor(customer*avg);
-      }
+
+ 
+
+
+
+   ////////////////////////////// DOM Element///////////////////
 
      var parentElement= document.getElementById('parentElement');// creating a  variable parentElement that represent the id of the section element
      var h1 = document.createElement('h1');  //creating a h1 element
-      h1.textContent = ' Welcome To The Salomon Cookie Store  '
+      h1.textContent = ' Welcome To The Salmon Cookie Store  ';
       parentElement.appendChild(h1); 
 
 
       var logo = document.createElement('img'); //creating img element
-      logo.setAttribute('src', '/images/salmon.png'); // adding the source 
+      logo.setAttribute('src', 'images/salmon.png'); // adding the source 
       parentElement.appendChild(logo);
   
-      var article = document.createElement('article');// creating the article element
-      parentElement.appendChild(article);
+      //var article = document.createElement('article');// creating the article element
+     // parentElement.appendChild(article);
 
-      for( var i=0;i<storesArray.length ;i++){// for loop that go through the array of store objects
- 
-      var h2 = document.createElement('h2');// creating an h2 element for each object 
-      h2.textContent = storesArray[i].storeName; // 
-      article.appendChild(h2);
-      var ul = document.createElement('ul');// creating a ul element 
-      article.appendChild(ul);
-      for(var x = 0; x <= storesArray[i].cookiesHour.length; x++){// loop through the cookieshour array
-          var li = document.createElement('li');// creating an li element
-          if(x=== storesArray[i].cookiesHour.length){//  add the total to the and of the list  
-            li.textContent =  time[x]+storesArray[i].dailyTotal;
-          }
-         else{ li.textContent =  time[x]+storesArray[i].cookiesHour[x] ; 
+      var storeTable=document.getElementById('storeTable');
+      var thead=document.createElement('thead');
+           storeTable.appendChild(thead);
+      var tr=document.createElement('tr');
+          thead.appendChild(tr);
          
-        }
-        ul.appendChild(li);
-    } 
-    }
+      var th=document.createElement('th');
+          tr.appendChild(th);
+
+      for (var i=0;i<time.length;i++){
+
+          th=document.createElement('th');
+          th.textContent=time[i];
+          tr.appendChild(th);
+
+      }
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+         //  if(x=== this.cookiesHour.length)
+         //  storecell.textContent=this.dailyTotal;
+         //        else
+         //        {
+         //           storecell.textContent=this.cookiesHour[i];
+         //       }
+         //  }
+      
+            
+   
+   
+
+
+
+
+
+
+
+      //{// for loop that go through the array of store objects   
+      // var h2 = document.createElement('h2');// creating an h2 element for each object 
+      // h2.textContent = storesArray[i].store.Name; // 
+      // article.appendChild(h2);
+      // var ul = document.createElement('ul');// creating a ul element 
+      // article.appendChild(ul);
+      // for(var x = 0; x <= storesArray[i].cookiesHour.length; x++){// loop through the cookieshour array
+      //     var li = document.createElement('li');// creating an li element
+      //     if(x=== storesArray[i].cookiesHour.length){//  add the total to the and of the list  
+      //       li.textContent =  time[x]+storesArray[i].dailyTotal;
+      //     }
+      //   
+         
+      //   }
+      //   ul.appendChild(li);
+   //  } 
+   //  }
